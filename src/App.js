bf46233e801f561836bd0686195e497154a2dba0
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-12-11 11:47:43 -0500

    deploy

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HeadBar from './components/HeadBar'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <HeadBar />
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
